001737c9fe51c5d9167d61454cdd2d720e24b604
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-23 13:41:01 +0300

    count button working

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -8,6 +8,13 @@ export class FormComponent extends Component {
       this.state = {
         count: 0
       }
+      this.handleCount = this.handleCount.bind(this) 
+    }
+
+    handleCount(){
+        this.setState({
+            count: this.state.count + 1
+        })
     }
 
 
@@ -20,7 +27,7 @@ export class FormComponent extends Component {
         </form>
         <button>Submit</button>
 
-        <button>Count ({this.state.count})</button>  
+        <button onClick = {this.handleCount}>Count ({this.state.count})</button>  
       </div>
     )
   }
